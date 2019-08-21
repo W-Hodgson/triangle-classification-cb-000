860b29a2364372383df026c2f7168b1c424faca2
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 10:05:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def postive_sides?
+    @a > 0 && @b > 0 && @c > 0
+  end
+
+  def triangle_inequality?
+    @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+  end
+
+  def equilateral?
+    @a == @b && @a == @c
+  end
+
+  def isosceles?
+    @a == @b || @a == @c || @b == @c
+  end
+
+  def kind
+    if postive_sides?
+      unless triangle_inequality?
+        if equilateral?
+          :equilateral
+        elsif isosceles?
+          :isosceles
+        else
+          :scalene
+        end
+      else
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle."
+    end
+  end
 end
